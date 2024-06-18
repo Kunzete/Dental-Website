84f5860b4bb7307c6b15beee5e63732ec835e26a
Kunzete <newkunzete@gmail.com>
2024-06-18 17:55:00 +0500

    compatible for all browser

--- assets/script.js
+++ assets/script.js
@@ -2,33 +2,33 @@ document.addEventListener('DOMContentLoaded', function () {
   const animatedNumbers = document.querySelectorAll('.animated-number');
 
   animatedNumbers.forEach(number => {
-      const target = parseInt(number.getAttribute('data-target'));
-      let current = 0;
-      const increment = Math.ceil(target / 90); // Adjust the speed here
+    const target = parseInt(number.getAttribute('data-target'));
+    let current = 0;
+    const increment = Math.ceil(target / 90); // Adjust the speed here
 
-      const updateNumber = () => {
-          if (current < target) {
-              current += increment;
-              if (current > target) {
-                  current = target;
-              }
-              number.textContent = current;
-              requestAnimationFrame(updateNumber);
-          }
-      };
+    const updateNumber = () => {
+      if (current < target) {
+        current += increment;
+        if (current > target) {
+          current = target;
+        }
+        number.textContent = current;
+        requestAnimationFrame(updateNumber);
+      }
+    };
 
-      updateNumber();
+    updateNumber();
   });
 });
 
-window.addEventListener('load', function() {
-    // Hide the preloader
-    document.getElementById('preloader').style.display = 'none';
-    // Show the content
-    document.getElementById('content').style.display = 'block';
+window.addEventListener('load', function () {
+  // Hide the preloader
+  document.getElementById('preloader').style.display = 'none';
+  // Show the content
+  document.getElementById('content').style.display = 'block';
 });
 
-      
+
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
 const close = document.getElementById("close");
